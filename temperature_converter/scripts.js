6e66bf197ffe7964ef56673390c7d3e09045724c
Karlygach Shaiykova <kshaiykova@gmail.com>
2020-08-15 01:45:47 -0700

    add age calculator & temperature converter homeworks

---
+++ temperature_converter/scripts.js
@@ -0,0 +1,20 @@
+var celsius = 30;
+var converttof = celsius * 9 /5 + 32;
+console.log (celsius + 'C\u00B0 is ' + converttof + 'F\u00B0');
+
+
+var farenhait = converttof;
+var convertocelsius=(farenhait - 32) * 5 /9;
+console.log(farenhait + 'F\u00B0 is ' + convertocelsius + 'C\u00B0');
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ the_age_calculator/scripts.js
@@ -0,0 +1,9 @@
+var currentyear = 2020;
+var birthdate = 1960;
+var firstage = currentyear - birthdate;
+var secondage = currentyear - birthdate - 1;
+console.log("They are either " + firstage + " or " + secondage);
+
+
+
+
